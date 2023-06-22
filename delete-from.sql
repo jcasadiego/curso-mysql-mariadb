fa85dff2509cb5d52110e664f3f48e8b495e14ea
jcasadiego <jcasadiego.a@gmail.com>
2023-06-22 08:00:18 -0400

    eliminar datos de una tabla, restablecer ids, y eliminar una tabla

---
+++ delete-from.sql
@@ -0,0 +1,5 @@
+USE metro_cdmx;
+
+-- Modo DELETE FROM
+
+DELETE FROM `stations_delete`;
---
+++ truncate.sql
@@ -0,0 +1,13 @@
+USE metro_cdmx;
+
+-- Modo TRUNCATE
+
+TRUNCATE TABLE `stations_delete`;
+
+-- Inserta las estaciones del metro
+INSERT INTO `stations_delete` (name) VALUES
+("Lázaro Cárdens"), -- Lázaro Cárdenas
+("Ferería"), -- Ferrería
+("Pnttlán"), -- Pantitlán
+("Tauga"), -- Tacuba
+("MartínCrrera"); -- Martín Carrera
