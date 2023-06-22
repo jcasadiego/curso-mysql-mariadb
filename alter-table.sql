44b95430c362b8e6f30950b6a5daab616a836241
jcasadiego <jcasadiego.a@gmail.com>
2023-06-22 07:05:12 -0400

    creando tablas stations, trains y stations_delete, ademas de modificar una tabla ya creada

---
+++ alter-table.sql
@@ -0,0 +1,6 @@
+USE metro_cdmx;
+
+ALTER TABLE `stations`
+MODIFY `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+-- ADD CONSTRAINT `trains_line_id_foreign`
+-- FOREIGN KEY (`line_id`) REFERENCES `lines`(`id`);
